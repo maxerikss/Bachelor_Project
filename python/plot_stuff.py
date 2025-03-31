581dfe41873ccec1743fc2966f3771f343c270a3
Max Eriksson <maxerikss@gmail.com>
2025-03-31 17:07:28 +0200

    fixed figure and started addressing feedback from Kalle

--- python/plot_stuff.py
+++ python/plot_stuff.py
@@ -24,6 +24,7 @@ def p2(omega, lamda, gamma, kbT):
 def E(omega, lamda, gamma, kbT):
     return omega/2 * (p2(omega, lamda, gamma, kbT) + x2(omega, lamda, gamma, kbT))
 
+
 #%%
 # Plotting x^2 vs lambda with varying Q factor
 
@@ -47,7 +48,7 @@ plt.savefig("../Bachelor_Thesis/figures/x2_vs_lambda.pdf", bbox_inches="tight")
 
 omega = 1
 lamda = [0.5, 1, 1.5]
-gamma = np.linspace(0.1, 1.8, 200)
+gamma = np.linspace(0.2, 1.5, 200)
 kbT = 10
 
 fig, ax = plt.subplots(1,1)
@@ -58,7 +59,7 @@ for l in lamda:
 ax.set_xlabel(r"$Q$")
 ax.set_ylabel(r"$\langle \tilde{x}^2 \rangle$")
 ax.legend()
-plt.savefig("../Bachelor_Thesis/figures/x2_vs_Q.pdf", bbox_inches="tight")
+#plt.savefig("../Bachelor_Thesis/figures/x2_vs_Q.pdf", bbox_inches="tight")
 #%%
 # Plotting p^2 against lambda with varying Q factor
 omega = 1
@@ -99,15 +100,15 @@ lamda = np.linspace(0, 2, 200)
 kbT = 10
 
 fig, ax = plt.subplots(1,1)
+fig.set_size_inches(6,4)
 
 for g in gamma:
     ax.plot(lamda/omega, E(omega, lamda, g, kbT), label = fr"$Q = {omega/g:.2f}$")
 
 ax.set_xlabel(r"$\lambda / \omega$")
 ax.set_ylabel(r"$\langle \tilde{E} \rangle$")
+ax.text(0.5, 14, "a", fontsize=20)
 ax.legend()
-plt.title(r"$E = \frac{\hbar \omega}{2} \left( \langle\tilde{p}^2 \rangle + \langle\tilde{x}^2\rangle \right)$ with $k_\mathrm{B}T = 10$",
-          pad=10)
 plt.savefig("../Bachelor_Thesis/figures/E_vs_lambda.pdf", bbox_inches="tight")
 #%%
 # Plotting E against Q with varying lambda
@@ -117,13 +118,15 @@ gamma = np.linspace(0.1, 1.8, 200)
 kbT = 10
 
 fig, ax = plt.subplots(1,1)
+fig.set_size_inches(6,4)
 
 for l in lamda:
     ax.plot(omega/gamma, E(omega, l, gamma, kbT), label = fr"$\lambda = {l:.2f}$")
 
 ax.set_xlabel(r"$Q$")
 ax.set_ylabel(r"$\langle \tilde{E} \rangle$")
+ax.text(0.5, 14, "b", fontsize=20)
 ax.legend()
-plt.title(r"$E = \frac{\hbar \omega}{2} \left( \langle\tilde{p}^2 \rangle + \langle\tilde{x}^2\rangle \right)$ with $k_\mathrm{B}T = 10$",
-          pad=10)
 plt.savefig("../Bachelor_Thesis/figures/E_vs_Q.pdf", bbox_inches="tight")
+
+# %%
